e67b8e6c7ebc7fa9bc9a3f4a5c0355b06aa75f83
Kudzai Mabika <kudzaimabika0109@gmail.com>
2022-06-30 01:41:07 +0200

    optmize fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,10 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -9,30 +15,35 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: [
-            <>
-              {initialProps.styles}
-              {sheet.getStyleElement()}
-            </>,
-          ],
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
       };
     } finally {
       sheet.seal();
     }
   }
-  render(){
-      return(
-        <Html lang="en">
+  render() {
+    return (
+      <Html lang="en">
         <Head>
           <meta property="og:type" content="website" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous | use-credentials"/>
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;400;500;700&family=Staatliches&display=swap"
             rel="stylesheet"
@@ -43,6 +54,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-      )
+    );
   }
 }
